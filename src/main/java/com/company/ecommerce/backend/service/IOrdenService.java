056976ccb86867abbbfc3c56ef040158f740e1df
Hector Lira Mancilla <hectormancilla489@gmail.com>
2023-12-13 17:55:02 -0600

    Metodo para Generar el numeo de la orden

--- src/main/java/com/company/ecommerce/backend/service/IOrdenService.java
+++ src/main/java/com/company/ecommerce/backend/service/IOrdenService.java
@@ -1,11 +1,14 @@
 package com.company.ecommerce.backend.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.company.ecommerce.backend.model.Orden;
 
 @Service
 public interface IOrdenService {
+	List<Orden> findAll();
 	Orden save(Orden orden);
 
 }
